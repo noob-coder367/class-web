e5180be881ade78c8b76681abcc98db4c1c065c4
noob-coder367 <phamtung23052011@gmail.com>
2026-09-10 19:32:17 +0700

    Wire WeatherProvider + LocationPermissionModal into App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import { AuthProvider } from './context/AuthContext.jsx'
+import { WeatherProvider } from './context/WeatherContext.jsx'
 import HomePage from './pages/HomePage.jsx'
 import OceanScrollBackground from './components/OceanScrollBackground.jsx'
+import LocationPermissionModal from './components/LocationPermissionModal.jsx'
 import './App.css'
 
 export default function App() {
   return (
     <AuthProvider>
-      <OceanScrollBackground />
-      <HomePage />
+      <WeatherProvider>
+        <OceanScrollBackground />
+        <HomePage />
+        <LocationPermissionModal />
+      </WeatherProvider>
     </AuthProvider>
   )
 }
